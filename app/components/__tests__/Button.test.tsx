8e6a9781602ccacc5bafa7c89c008a41b6390fa2
Matheus Rocha Sousa <166670004+mrsMatheusRocha@users.noreply.github.com>
2026-01-22 22:20:29 -0300

    UI testes integrated with Vitest

---
+++ app/components/__tests__/Button.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import { describe, it, expect, vi } from 'vitest'
+import Button from '../ui/Button';
+
+describe('Button Component', () => {
+  it('renders correctly', () => {
+    render(<Button>Click me</Button>)
+    expect(screen.getByText('Click me')).toBeInTheDocument()
+  })
+
+  it('calls onClick handler when clicked', () => {
+    const handleClick = vi.fn()
+    render(<Button onClick={handleClick}>Click me</Button>)
+
+    fireEvent.click(screen.getByText('Click me'))
+    expect(handleClick).toHaveBeenCalledTimes(1)
+  })
+})
